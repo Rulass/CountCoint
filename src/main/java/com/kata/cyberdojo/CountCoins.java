ccf189cbfcd0475599f389f8c7e29fde8e9dbc4e
Rulass <rockrulas54@gamil.com>
2014-10-16 18:10:15 -0500

    version errornea

--- src/main/java/com/kata/cyberdojo/CountCoins.java
+++ src/main/java/com/kata/cyberdojo/CountCoins.java
@@ -2,9 +2,32 @@ package com.kata.cyberdojo;
 
 public class CountCoins {
 
-	public String changeOf(int i) {
-		// TODO Auto-generated method stub
-		return null;
+	public String changeOf(int end) {
+		String result ="";
+		for (int i=1 ; i<=end ; i++){
+			if (i <= end){
+				result += "1 pennie";
+			}else if(i==5){
+			return "1 nickel o 5 pennie";
+			}else if(i==6){
+			return "1 nickel y 1 pennie o 6 pennie";
+			}else	if(i==7){
+			return "1 nickel y 2 pennie o 7 pennie";
+			}else	if(i==8){
+			return "1 nickel y 3 pennie o 8 pennie";
+			}else	if(i==9){
+			return "1 nickel y 4 pennie o 9 pennie";
+			}else	if(i==10){
+			return "1 dime o 2 niquel o 1 niquel y 5 pennie o 10 penies";
+		}else	if(i==11){
+			return "1 dime y 1 pennie o 2 niquel y 1 pennie o 1 niquel y 6 pennie o 11 penies";
+		}else	if(i==25){
+			return "1 quarter";
+		}else{
+			result += i;
+			}
+		}
+		return  result;
 	}
 
 }
--- src/test/java/com/kata/cyberdojo/CountCoinTest.java
+++ src/test/java/com/kata/cyberdojo/CountCoinTest.java
@@ -1,8 +1,8 @@
 package com.kata.cyberdojo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class CountCoinTest {
@@ -14,5 +14,68 @@ public class CountCoinTest {
 		String result = countCoins.changeOf(1);
 		assertEquals(expected, result);
 	}
-
+	@Test
+	public void testCalulateCoinsFor2Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "2 pennie";
+		String result = countCoins.changeOf(2);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor3Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "3 pennie";
+		String result = countCoins.changeOf(3);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor4Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "4 pennie";
+		String result = countCoins.changeOf(4);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor5Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 nickel o 5 pennie";
+		String result = countCoins.changeOf(5);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor6Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 nickel y 1 pennie o 6 pennie";
+		String result = countCoins.changeOf(6);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor7Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 nickel y 2 pennie o 7 pennie";
+		String result = countCoins.changeOf(7);
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testCalulateCoinsFor10Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 dime o 2 niquel o 1 niquel y 5 pennie o 10 penies";
+		String result = countCoins.changeOf(10);
+		assertEquals(expected, result);
+	}
+	@Test 
+	public void testCalulateCoinsFor11Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 dime y 1 pennie o 2 niquel y 1 pennie o 1 niquel y 6 pennie o 11 penies";
+		String result = countCoins.changeOf(11);
+		assertEquals(expected, result);
+	}
+	@Test
+	public void testCalulateCoinsFor25Cent() {
+		CountCoins countCoins = new CountCoins();
+		String expected = "1 quarter";
+		String result = countCoins.changeOf(25);
+		assertEquals(expected, result);
+	}
 }
